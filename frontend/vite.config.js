338030b13effb3d50ef6d9b46aae929da414c4da
ablas123 <67266977+ablas123@users.noreply.github.com>
2026-03-12 22:47:51 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/clinic-management-system/frontend/',  // ⚠️ مهم لـ GitHub Pages
+  base: '/clinic-management-system/',
   server: {
     port: 5173,
     open: true
